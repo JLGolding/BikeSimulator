75f496d49090488d0657c83ffefd023ecde96fc6
James Golding <jamesgolding6@gmail.com>
2023-02-05 13:30:53 +1100

    naming improvement

--- app/src/main/java/bikesimulator/Simulator.java
+++ app/src/main/java/bikesimulator/Simulator.java
@@ -30,26 +30,26 @@ public class Simulator {
             var input = reader.readInput();
             if(input.equals("q")) running = false;
             var elements = input.split(" ");
-            var command = elements[0];
-            if(inputValidator.validate(command)){
-                if(command.equals("PLACE")){
+            var commandInput = elements[0];
+            if(inputValidator.validate(commandInput)){
+                if(commandInput.equals("PLACE")){
                     var points = elements[1].split(",");
                     area.placeBike(Integer.parseInt(points[0]), Integer.parseInt(points[1]));
                     bike.setDirection(Directions.valueOf(points[2]));
                     if(bikePlacedInitially == false) bikePlacedInitially = true;
                 }
                 if(bikePlacedInitially){
-                    if(command.equals("GPS_REPORT")){
+                    if(commandInput.equals("GPS_REPORT")){
                         var bikePosition = area.getBikePosition();
                         String output = MessageFormat.format("({0},{1}), {2}", 
                         bikePosition.getXPosition(), bikePosition.getYPosition(), bike.getFacingDirection().toString());
                         writer.Print(output);
                     }
-                    if(command.contains("TURN")){
-                        var direction = command.split("_")[1];
+                    if(commandInput.contains("TURN")){
+                        var direction = commandInput.split("_")[1];
                         bike.turn(Directions.valueOf(direction));    
                     }
-                    if(command.equals("FORWARD")){
+                    if(commandInput.equals("FORWARD")){
                         area.moveBike(bike);
                     }
                 }
